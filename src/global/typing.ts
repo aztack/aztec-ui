a0548fcaba738a0d2499ca938f225b65ad478225
wangweihua <wangweihua@bytedance.com>
2021-07-07 10:53:56 +0800

    feat(typing):✨ add Modifer related types

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -1,3 +1,5 @@
+import { PermutationOfCombination } from "../utils/type-utils";
+
 export type ComponentStyle = 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
 export type ComponentSize = 'extra-small' | 'small' | 'normal' | 'medium' | 'large' | 'extra-large';
 
@@ -6,5 +8,8 @@ export type EdgePlacement = 'top-center' | 'bottom-center' | 'right-center' | 'l
 export type CornerPlacement = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
 export type Placement = EdgePlacement | CornerPlacement | CenterPlacement;
 
-export type PositionHorizontal = 'left' | 'right';
-export type PositionVertical = 'top' | 'bottom';
+export type PositionHorizontal = 'left' | 'right' | CenterPlacement;
+export type PositionVertical = 'top' | 'bottom' | CenterPlacement;
+
+export type ModifierKeys = 'meta' | 'ctrl' | 'shift' | 'alt';
+export type ModifierCombinations = '' | PermutationOfCombination<ModifierKeys>;
