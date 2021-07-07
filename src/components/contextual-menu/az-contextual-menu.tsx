efb92f9591e3938746577fbfa89e17dffec87f4a
wangweihua <wangweihua@bytedance.com>
2021-07-07 19:09:58 +0800

    fix(az-contextual-menu):🐛 fix contextual-menu positioning bug

--- src/components/contextual-menu/az-contextual-menu.tsx
+++ src/components/contextual-menu/az-contextual-menu.tsx
@@ -1,5 +1,6 @@
 import { Component, Prop, Element, Host, Event, EventEmitter, h } from '@stencil/core';
-import { Inject } from '../../utils';
+import { ModifierCombinations } from '../../global/typing';
+import { Inject, isModiferKeysPressed } from '../../utils';
 
 @Component({
   tag: 'az-contextual-menu',
@@ -11,22 +12,26 @@ export class AzContextMenu {
 
   @Prop({reflect: true}) caption: string = '';
   @Prop({reflect: true}) triggerevent: string = 'contextmenu';
-  @Prop({reflect: true}) closeevent: string = '';
+  @Prop({reflect: true}) modifiers: ModifierCombinations = '';
+  @Prop({reflect: true}) closeevent: string = 'blur';
   @Prop({reflect: true}) parent: string = 'body';
-  @Prop({reflect: true, mutable: true}) popupalign: string = 'bottom left';
-  @Prop({reflect: true}) closedelay: number = 500;
+  @Prop({reflect: true, mutable: true}) popupalign: string = '';
+  @Prop({reflect: true}) closedelay: number = 100;
 
   @Event() showed: EventEmitter;
-  container: HTMLElement;
+  @Event() hidden: EventEmitter;
+  isSubMenu: boolean = false;
 
   @Inject({
     sync: ['show', 'hide', 'items',  'insertItem', 'removeItem']
   })
   componentDidLoad() {
-    this.container = this.el.parentElement;
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
     let parent = this.el.parentElement;
+    if (parent.tagName === 'AZ-MENU-ITEM') {
+      this.isSubMenu = true;
+    }
 
     let hideTimer = -1;
     if (this.triggerevent) {
@@ -36,6 +41,7 @@ export class AzContextMenu {
           hideTimer = -1;
         }
         this.show(e);
+        e.stopPropagation();
       });
     }
     if (this.closeevent) {
@@ -48,31 +54,39 @@ export class AzContextMenu {
     });
     this.el.addEventListener('selected', this.hide);
 
-    if (this.parent != 'parent') {
+    if (!this.popupalign && !this.isSubMenu) {
+      // menu without popupalign should be move to `parent(body)`
+      // so that we can use e.pageX/Y when show
       parent = document.querySelector(this.parent);
       parent.appendChild(this.el);
     } else {
+      // parent of menu with popupalign set should be relative positioned
       parent.style.position = 'relative';
     }
   }
 
   show(e: MouseEvent) {
+    if (!isModiferKeysPressed(e, this.modifiers)) return;
     const styl = this.el.style;
     styl.display = 'flex';
-    if (e && !this.popupalign) {
+
+    if (e && !this.isSubMenu && !this.popupalign) {
       styl.left = e.pageX + 'px';
       styl.top = e.pageY + 'px';
-      e.preventDefault();
     }
+    if (e) e.preventDefault();
     this.showed.emit(e);
   }
 
   hide() {
     this.el.style.display = 'none';
+    this.hidden.emit();
   }
 
   items(includeSeparator: boolean = true) {
-    return includeSeparator ? this.el.querySelectorAll('az-menu-item') : this.el.querySelectorAll('az-menu-item:not(.separator)');
+    return includeSeparator
+      ? this.el.querySelectorAll('az-menu-item')
+      : this.el.querySelectorAll('az-menu-item:not(.separator)');
   }
 
   insertItem(index: number, caption: string, icon?: string, action?: string, includeSeparator?: boolean) {
@@ -90,7 +104,7 @@ export class AzContextMenu {
   removeItem(index: number, includeSeparator?: boolean) {
     const items = this.items(includeSeparator);
     if (index < 0) throw new RangeError('index must be greater than or equal to zero');
-    if (index >= items.length) throw new RangeError('index must be less than or equal to items length');
+    if (index >= items.length) throw new RangeError('index must be less than items length');
     const item = items[index];
     item.parentNode.removeChild(item);
   }
