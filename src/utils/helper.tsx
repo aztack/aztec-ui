84c90f12b2534b9e5b7438ab12944a412652be66
wangweihua <wangweihua@bytedance.com>
2021-07-07 10:54:54 +0800

    feat(helper):✨ add helper functions
    - setInternalInstance
    - getInternalInstance
    - isModifierKeysPressed

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,4 +1,5 @@
 import { h } from "@stencil/core";
+import { ModifierCombinations } from "../global/typing";
 
 export function getCaptionWithIcon(caption: string, icon: string = '', iconPosition: string = '', classes: string = '') {
   return <span class={`az-caption__wrapper ${classes}`}>
@@ -6,4 +7,23 @@ export function getCaptionWithIcon(caption: string, icon: string = '', iconPosit
     {caption && <span class={`az-button-caption az-caption`}>{caption}</span>}
     {(icon && iconPosition === 'right') ? <az-icon slot="after" icon={icon}></az-icon> : <slot name="after"></slot>}
   </span>;
+}
+
+const stencil = Symbol.for('__$stencil');
+
+export function setInternalInstance<T>(e: HTMLElement, instance: T) {
+  if (e) e[stencil] = instance;
+}
+
+export function getInternalInstance<T>(e: HTMLElement) {
+  if (!e) return null;
+  return e[stencil] as T;
+}
+
+export function isModiferKeysPressed(e: MouseEvent, modifiers: ModifierCombinations) {
+  if (!modifiers) return true;
+  for (const modifer of modifiers.split(' ')) {
+    if (!e[`${modifer}Key`]) return false;
+  }
+  return true;
 }
