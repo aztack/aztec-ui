d61404e8632111685b5de20d5ecfe487f36b6c5a
wangweihua <wangweihua@bytedance.com>
2021-07-07 19:12:16 +0800

    refactor(az-menu-item):♻️ make props reflect; add extraIcon, extraText props

--- src/components/menu-item/az-menu-item.tsx
+++ src/components/menu-item/az-menu-item.tsx
@@ -1,5 +1,6 @@
 import { Component, Prop, Element, Host, Event, EventEmitter, h } from '@stencil/core';
 import { ComponentStyle } from '../../global/typing';
+import { Inject } from '../../utils';
 
 @Component({
   tag: 'az-menu-item',
@@ -9,14 +10,19 @@ import { ComponentStyle } from '../../global/typing';
 export class AzMenuItem {
   @Element() el: HTMLElement;
 
-  @Prop() caption: string = '';
-  @Prop() icon: string = '';
-  @Prop() action: string = '';
-  @Prop() type: ComponentStyle = 'plain';
+  @Prop({reflect: true}) caption: string = '';
+  @Prop({reflect: true}) icon: string = '';
+  @Prop({reflect: true}) action: string = '';
+  @Prop({reflect: true}) type: ComponentStyle = 'plain';
+  @Prop({reflect: true}) extraIcon: string = '';
+  @Prop({reflect: true}) extraText: string = '';
 
   @Event() selected: EventEmitter;
 
-  componentDidLoad() {}
+  @Inject()
+  componentDidLoad() {
+    this.onClick = this.onClick.bind(this);
+  }
 
   onClick() {
     this.selected.emit(this.action);
@@ -25,16 +31,18 @@ export class AzMenuItem {
   render() {
     if (this.caption === '-') {
       return <Host class="az-menu-item az-menu-item__separator separator"></Host>
-    } else {
-      return (
-        <Host class={{
-          'az-menu-item': true,
-          [this.type]: true
-        }} onClick={() => this.onClick()}>
-          {this.icon && <az-icon icon={this.icon} />}
-          <span class="az-caption">{this.caption}</span>
-        </Host>
-      );
     }
+    let extra = null;
+    if (this.extraIcon) extra = <az-icon icon={this.extraIcon}></az-icon>;
+    if (this.extraText) extra = <span>{this.extraText}</span>;
+    return (
+      <Host class={`az-menu-item ${this.type}`} onClick={this.onClick}>
+        {this.icon && <az-icon icon={this.icon} />}
+        <span class="az-caption" title={this.caption}>{this.caption}</span>
+          <span class="az-menu-item__extra">
+            {extra || <slot name="extra"></slot>}
+          </span>
+      </Host>
+    );
   }
 }
