d1c4f8f0345e62eee13f123caf9daffab8157338
wangweihua <wangweihua@bytedance.com>
2021-07-09 20:00:13 +0800

    feat(typing):✨ add MouseButton type

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -13,3 +13,29 @@ export type PositionVertical = 'top' | 'bottom' | CenterPlacement;
 
 export type ModifierKeys = 'meta' | 'ctrl' | 'shift' | 'alt';
 export type ModifierCombinations = '' | PermutationOfCombination<ModifierKeys>;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+export enum MouseButton {
+  /**
+   * Main button pressed, usually the left button or the un-initialized state
+   */
+  Main = 0,
+  /**
+   * Auxiliary button pressed, usually the wheel button or the middle button (if present)
+   */
+  Auxiliary = 1,
+  /**
+   * Secondary button pressed, usually the right button
+   */
+  Secondary = 2,
+  /**
+   * Fourth button, typically the Browser Back button
+   */
+  Fourth = 3,
+  /**
+   * Fifth button, typically the Browser Forward button
+   */
+  Fifth = 4,
+};
+
+export type MouseOrKeyboardEvent = MouseEvent | KeyboardEvent;
