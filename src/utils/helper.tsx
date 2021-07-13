2c27960a814902189e452ad6354aa131216fe2fc
wangweihua <wangweihua@bytedance.com>
2021-07-13 20:21:11 +0800

    refactor(helper):♻️  getCaptionWithIcon add last props param

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,8 +1,8 @@
 import { h } from "@stencil/core";
 import { ModifierCombinations } from "../global/typing";
 
-export function getCaptionWithIcon(caption: string, icon: string = '', iconPosition: string = '', classes: string = '') {
-  return <span class={`az-caption__wrapper ${classes}`}>
+export function getCaptionWithIcon(caption: string, icon: string = '', iconPosition: string = '', classes: string = '', ...props) {
+  return <span class={`az-caption__wrapper ${classes}`} {...props}>
     {(icon && iconPosition === 'left') ? <az-icon slot="before" icon={icon}></az-icon> : <slot name="before"></slot>}
     {caption && <span class={`az-button-caption az-caption`}>{caption}</span>}
     {(icon && iconPosition === 'right') ? <az-icon slot="after" icon={icon}></az-icon> : <slot name="after"></slot>}
